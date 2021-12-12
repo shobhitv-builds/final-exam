f3a3a9991b599a62a62938b58c13337d6e2e2359
Shobhit <shobhitv@uchicago.edu>
2021-12-11 21:05:43 -0600

    Encode a file

--- src/main.c
+++ src/main.c
@@ -26,18 +26,33 @@ char** generateEncoding(int numChars, TreeNode* prefixTree){
     return encodings;
 }
 
-int main(const int argc, const char* argv[]){
-    int numChars = 0;
-    TreeNode* prefixTree = importFile(defaultFile, &numChars);
-    printTree(prefixTree);
-
-    char** encodings = generateEncoding(numChars, prefixTree);
-
+void printEncodings(char** encodings){
     for(int i = 0; i < ALPHABET_SIZE; i++){
         if(encodings[i] == NULL) continue;
         printf("%c encoded to %s\n", i, encodings[i]);
     }
+}
 
+void printEncodedFile(const char* fileName, char** encodings){
+    FILE* file = fopen(defaultFile, "r");
+    char c;
+    while( (c = fgetc(file)) != EOF ){
+        printf("%s", encodings[(int)c]);
+    }
+    fclose(file);
+}
+
+int main(const int argc, const char* argv[]){
+    int numChars = 0;
+    TreeNode* prefixTree = importFile(defaultFile, &numChars);
+    // printTree(prefixTree);
+
+    char** encodings = generateEncoding(numChars, prefixTree);
+    // printEncodings(encodings);
+
+    printEncodedFile(defaultFile, encodings);
+
+    for(int i = 0; i < ALPHABET_SIZE; i++) free(encodings[i]);
     destroyTree(prefixTree);
     return 0;
 }
